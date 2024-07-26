23972a08ead2f6b9c8cae489fc95318707a9ad25
Dongha Choi <carmania0701@gmail.com>
2024-07-26 13:28:59 +0900

    chore: apply fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <>
       <section>
-        <article className='ml-20 text-2xl'>
+        <article className='text-2xl'>
           Create a hood with your loved ones and record photos of the moment.
         </article>
       </section>
