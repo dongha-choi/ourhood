1dfd5d900b9da0245a5fd93f29dcabc5c51182ed
Dongha Choi <carmania0701@gmail.com>
2024-07-26 12:39:26 +0900

    chore: install tailwindcss

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
+    <nav className='flex gap-10'>
       <Link to='/'>home</Link>
       <Link to='/login'>login</Link>
       <Link to='/signup'>sign up</Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
   return (
     <>
       <section>
-        <article>
+        <article className='ml-20 text-2xl'>
           Create a hood with your loved ones and record photos of the moment.
         </article>
       </section>
